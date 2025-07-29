1fb986bc839f9a037fc54a51565ce99877dc82c0
Syahadi <54655295+AdiSyahadi@users.noreply.github.com>
2025-07-29 11:02:36 +0700

    Create data_customer.py

---
+++ data_customer/data_customer.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from streamlit_autorefresh import st_autorefresh
+
+def show_data_customer(df_customer):
+    st_autorefresh(interval=1000, key="datarefresh")
+    st.title("📗 TABEL DATA CUSTOMER")
+
+    st.subheader("⬇️ Export Data Customer")
+    selected_cols = st.multiselect(
+        "Pilih kolom yang ingin diekspor:",
+        options=df_customer.columns.tolist(),
+        default=df_customer.columns.tolist()
+    )
+
+    if selected_cols:
+        export_df = df_customer[selected_cols].drop_duplicates().dropna(how="all")
+        st.download_button(
+            label="💾 Download CSV",
+            data=export_df.to_csv(index=False).encode("utf-8"),
+            file_name="data_customer_terpilih.csv",
+            mime="text/csv"
+        )
+
+    st.dataframe(df_customer, use_container_width=True)
